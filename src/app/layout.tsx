822145a874b39045c95b6fb86a3494b1db4c7451
Yasi K'ari <lexusyasi@gmail.com>
2025-05-20 21:39:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <head>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/reminders" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    >                       <html lang="es">
    > >                       <head>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <link>
    >                           <link>
    >                           <link>
    >                           <link>
    > >                         {" "}
    >                           ...
    >                         ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (31:7) @ RootLayout
    >
    >   29 |   return (
    >   30 |     <html lang="es">
    > > 31 |       <head>
    >      |       ^
    >   32 |         <meta name="application-name" content="YASI K'ARI" />
    >   33 |         <meta name="apple-mobile-web-app-capable" content="yes" />
    >   34 |         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
    >
    > Call Stack
    > 14
    >
    > Show 12 ignore-listed frame(s)
    > head
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (31:7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,24 +36,20 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#526691" />
+        <meta name="msapplication-TileColor" content="#3F51B5" /> {/* Updated to match primary color */}
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#526691" />
-
+        <meta name="theme-color" content="#3F51B5" /> {/* Updated to match primary color */}
         {/* Add to homescreen for Chrome on Android. Fallback for manifest.json */}
         <link rel="manifest" href="/manifest.json" />
-
         {/* Apple Touch Icons */}
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" /> {/* Using 192 as a substitute for 180 */}
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-192x192.png" /> {/* Using 192 as a substitute for 167 */}
-
         {/* Favicon links - user needs to add these files */}
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
-
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
